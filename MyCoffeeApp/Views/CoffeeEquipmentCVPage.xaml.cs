b61dd527f7f79e5164eba1d68292185d2ee14c6c
Supradeep Mukherjee <supradeep2004@gmail.com>
2026-02-03 17:49:35 +0530

    SwipeView

--- MyCoffeeApp/Views/CoffeeEquipmentCVPage.xaml.cs
+++ MyCoffeeApp/Views/CoffeeEquipmentCVPage.xaml.cs
@@ -20,33 +20,29 @@ public partial class CoffeeEquipmentCVPage : ContentPage
     private async void SwipeItem_Invoked(object sender, EventArgs e)
     {
         var item = sender as SwipeItem;
-        if (item is null)
-            return;
+        if (item is null) return;
 
         var coffee = item.BindingContext as Coffee;
-        if (coffee is null)
-            return;
+        if (coffee is null) return;
 
-        await Shell.Current.DisplayAlert("Coffee Favorited",
+        await Shell.Current.DisplayAlertAsync("Coffee Favorited",
             coffee.Name, "OK");
     }
 
     private async void MenuFlyoutItem_Clicked(object sender, EventArgs e)
     {
         var item = sender as MenuFlyoutItem;
-        if (item is null)
-            return;
+        if (sender is null) return;
 
         var coffee = item.BindingContext as Coffee;
-        if (coffee is null)
-            return;
+        if (coffee is null) return;
 
-        await Shell.Current.DisplayAlert("Coffee Favorited",
+        await Shell.Current.DisplayAlertAsync("Coffee Favorited",
             coffee.Name, "OK");
     }
 
     private async void MenuFlyoutItem_Clicked_1(object sender, EventArgs e)
     {
-        await Shell.Current.DisplayAlert("Menu item clicked!", "You did it!", "OK");
+        await Shell.Current.DisplayAlertAsync("Menu item clicked!", "You did it!", "OK");
     }
 }
